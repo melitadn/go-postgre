d8a1e660f041d7671a4173e93017321f4eb47c5a
Melita Dwi Nugroho <melita10dn@gmail.com>
2022-08-12 12:52:59 +0700

    add / to delete path

--- main.go
+++ main.go
@@ -115,7 +115,7 @@ func (r *Repository) GetBookByID(context *fiber.Ctx) error {
 func (r *Repository) SetupRoutes(app *fiber.App) {
 	api := app.Group("/api")
 	api.Post("/create_books", r.CreateBook)
-	api.Delete("delete_book/:id", r.DeleteBook)
+	api.Delete("/delete_book/:id", r.DeleteBook)
 	api.Get("/get_books/:id", r.GetBookByID)
 	api.Get("/books", r.GetBooks)
 }
